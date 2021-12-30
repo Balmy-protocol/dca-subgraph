14b1fff1659443ec5d0c4f1e96a10a706c46a301
Sebastian Torres <seba.j.torres@gmail.com>
2021-12-30 18:02:51 -0300

    feat(positions): add transfer support

--- src/mappings/permissionsManager.ts
+++ src/mappings/permissionsManager.ts
@@ -1,8 +1,10 @@
 import * as transactionLibrary from '../utils/transaction';
+import * as positionLibrary from '../utils/position';
 import { Approval, ApprovalForAll, Modified, Transfer } from '../../generated/PermissionsManager/PermissionsManager';
 
 export function handleTransfer(event: Transfer): void {
   let transaction = transactionLibrary.getOrCreateFromEvent(event, 'Transfer');
+  positionLibrary.transfer(event, transaction);
 }
 
 export function handleApproval(event: Approval): void {
