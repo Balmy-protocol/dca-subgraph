3e964227f40dbeed3c56cb1086783718d8980f42
Ged <0xged@protonmail.com>
2022-09-16 20:05:46 -0300

    chore: checkpoint

--- src/utils/pair-swap.ts
+++ src/utils/pair-swap.ts
@@ -38,19 +38,19 @@ export function create(pair: Pair, swappedPairInformation: SwappedSwapInformatio
   return pairSwap;
 }
 
-export function mapTokensInSwap(event: Swapped): Map<Address, SwappedSwapInformationTokensStruct> {
+export function getPlatformFeeMap(event: Swapped): PlatformFeeMap {
   log.debug('[PairSwap] Map tokens in swap', []);
-  const tokensInSwapMapped = new Map<Address, SwappedSwapInformationTokensStruct>();
+  const tokensInSwapMapped = new PlatformFeeMap();
   const tokens = event.params.swapInformation.tokens;
   for (let i: i32 = 0; i < tokens.length; i++) {
-    tokensInSwapMapped.set(tokens[i].token, tokens[i]);
+    tokensInSwapMapped.addPlatformFee(tokens[i].token, tokens[i].platformFee);
   }
   return tokensInSwapMapped;
 }
 
 export function getExtendedPairInformation(event: Swapped): ExtendedPairInformation[] {
   log.debug('[PairSwap] Get extended pair information', []);
-  const tokensInSwap = mapTokensInSwap(event);
+  const platformFeeByTokens = getPlatformFeeMap(event);
   const neededPerToken = new Map<Address, BigInt>();
   const extendedPairInformation: ExtendedPairInformation[] = [];
   const pairs = event.params.swapInformation.pairs;
@@ -74,42 +74,43 @@ export function getExtendedPairInformation(event: Swapped): ExtendedPairInformat
     );
 
     extendedPairInformation.push(new ExtendedPairInformation(neededA, neededB, ZERO_BI, ZERO_BI));
-    log.warning('needed per token A {}', [currentNeededA.plus(neededA).toHexString()]);
     neededPerToken.set(tokenA, currentNeededA.plus(neededA));
-    log.warning('get needed per token A {}', [neededPerToken.get(tokenA)!.toHexString()]);
     neededPerToken.set(tokenB, currentNeededB.plus(neededB));
   }
-  log.debug('pre platform fee', []);
+  log.warning('pre platform fee', []);
   for (let i: i32 = 0; i < pairs.length; i++) {
     const tokenA = pairs[i].tokenA;
     const tokenB = pairs[i].tokenB;
-    log.debug('pb {} {} pa {}', [
-      tokenB.toHexString(),
-      tokensInSwap.has(tokenB) ? 'true' : 'false',
-      tokensInSwap.has(tokenA) ? 'true' : 'false',
-    ]);
+    // log.error('hash {} ta {} tb {} ta by index {} tb by index {}', [
+    //   event.transaction.hash.toHexString(),
+    //   platformFeeByTokens.has(tokenA) ? 'true' : 'false',
+    //   platformFeeByTokens.has(tokenB) ? 'true' : 'false',
+    //   tokensAddresses.indexOf(tokenA).toString(),
+    //   tokensAddresses.indexOf(tokenB).toString()
+    // ]);
+    // if (!platformFeeByTokens.has(tokenA)) log.error('ta {}', [tokenA.toHexString()]);
+    // if (!platformFeeByTokens.has(tokenB)) log.error('tb {}', [tokenB.toHexString()]);
+    // log.error(' - ', []);
+    // log.error(' - ', []);
+    // log.error(' - ', []);
 
     // totalNeeded(tokenB)    - platformFee(tokenB)
     // needed(tokenB, pairAB) - platformFeeAtoB = needed(tokenB, pairAB) * platformFee(tokenB) / totalNeeded(tokenB)
     extendedPairInformation[i].platformFeeAToB = extendedPairInformation[i].neededTokenB
-      .times(tokensInSwap.get(tokenB).platformFee)
+      .times(platformFeeByTokens.getPlatformFee(tokenB))
       .div(neededPerToken.get(tokenB));
     // totalNeeded(tokenA)    - platformFee(tokenA)
     // needed(tokenA, pairAB) - platformFeeBtoA = needed(tokenA, pairAB) * platformFee(tokenA) / totalNeeded(tokenA)
     extendedPairInformation[i].platformFeeBToA = extendedPairInformation[i].neededTokenA
-      .times(tokensInSwap.get(tokenA).platformFee)
+      .times(platformFeeByTokens.getPlatformFee(tokenA))
       .div(neededPerToken.get(tokenA));
   }
-  log.debug('post platform fee', []);
+  log.warning('post platform fee', []);
   return extendedPairInformation;
 }
 
 // On-chain ref.: https://github.com/Mean-Finance/dca-v2-core/blob/main/contracts/DCAHub/DCAHubSwapHandler.sol#L55
 export function convertAndApplyFee(from: Token, amount: BigInt, ratioFromTo: BigInt, fee: BigInt): BigInt {
-  // uint256 _numerator = FeeMath.subtractFeeFromAmount(_swapFee, _amountFrom * _rateFromTo);
-  // _amountTo = _numerator / _fromTokenMagnitude;
-  // // Note: we need to round up because we can't ask for less than what we actually need
-  // if (_numerator % _fromTokenMagnitude != 0) _amountTo++;
   const numerator = applyFee(fee, amount.times(ratioFromTo));
   let amountTo = numerator.div(from.magnitude);
   if (numerator.mod(from.magnitude).notEqual(ZERO_BI)) amountTo = amountTo.plus(ONE_BI);
@@ -119,7 +120,6 @@ export function convertAndApplyFee(from: Token, amount: BigInt, ratioFromTo: Big
 // On-chain ref.: https://github.com/Mean-Finance/dca-v2-core/blob/0fc7a600604c694e22b35bed53d7c7a12b8bca10/contracts/libraries/FeeMath.sol#L30
 export function applyFee(fee: BigInt, amount: BigInt): BigInt {
   const FEE_PRECISION = BigInt.fromI32(10000);
-  // (_amount * (FEE_PRECISION - _fee / 100)) / FEE_PRECISION;
   return amount.times(FEE_PRECISION.minus(fee.div(BigInt.fromI32(100)))).div(FEE_PRECISION);
 }
 
@@ -128,4 +128,24 @@ export function applyFeeWithExtraPrecision(fee: BigInt, amount: BigInt): BigInt 
   const FEE_PRECISION = BigInt.fromI32(10000);
   const feeAmount = amount.times(FEE_PRECISION.minus(fee.div(BigInt.fromI32(100))));
   return amount.minus(feeAmount);
+}
+
+export class PlatformFeeMap {
+  private _tokensAddresses: Address[];
+  private _platformFees: BigInt[];
+
+  constructor() {
+    this._tokensAddresses = [];
+    this._platformFees = [];
+  }
+
+  addPlatformFee(token: Address, platformFee: BigInt): void {
+    this._tokensAddresses.push(token);
+    this._platformFees.push(platformFee);
+  }
+
+  getPlatformFee(tokenAddress: Address): BigInt {
+    const indexOfFee = this._tokensAddresses.indexOf(tokenAddress);
+    return this._platformFees[indexOfFee];
+  }
 }
